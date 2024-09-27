74965a9dd056afdae3a0efb2e1d82dc37b0e28f2
diamondl53 <diamondl53@nycstudents.net>
2024-09-27 02:45:49 +0000

    commit

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -1,20 +1,29 @@
 package com.example.project;
 
+import java.text.DecimalFormat;
+
 public class TipCalculator {
     //WRITE YOUR PROGRAM IN calculateTip
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
-
-                       
+        double totalBill = cost;
+        double totalTip = totalBill*(percent/100.0);
+        double billwithTip = totalBill+totalTip;
+        double personCost = totalBill/people;
+        double personTip = totalTip/people;
+        double personcostwithTip = personCost+personTip;
+        String pattern = "##.##";
+        //Here is where I researched rounding: https://jenkov.com/tutorials/java-internationalization/decimalformat.html//
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: "); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: ");
-        result.append("Total tip: ");
-        result.append("Total Bill with tip: ");
-        result.append("Per person cost before tip: ");
-        result.append("Tip per person: ");
-        result.append("Total cost per person: ");
+        result.append("Total bill before tip: $"+ decimalFormat.format(totalBill) + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: "  + decimalFormat.format(percent) + "%\n");
+        result.append("Total tip: $" + decimalFormat.format(totalTip) + "\n");
+        result.append("Total Bill with tip: $" + decimalFormat.format(billwithTip) + "\n");
+        result.append("Per person cost before tip: $" + decimalFormat.format(personCost) + "\n");
+        result.append("Tip per person: $" +decimalFormat.format(personTip) + "\n");
+        result.append("Total cost per person: $" + decimalFormat.format(personcostwithTip) + "\n");
         result.append("-------------------------------\n");
 
 
@@ -23,9 +32,9 @@ public class TipCalculator {
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people; 
-        int percent;
-        double cost;              
+        int people = 6; 
+        int percent = 25;
+        double cost = 52.27;              
         System.out.println(calculateTip(people,percent,cost));
     }
 }
